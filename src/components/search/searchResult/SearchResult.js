32f7dbaa52a87e05067aca30971bc5e153cac6ea
damienmende30 <damienmende30@gmail.com>
2022-01-12 15:20:20 +0200

    Grid data from api

--- src/components/search/searchResult/SearchResult.js
+++ src/components/search/searchResult/SearchResult.js
@@ -1,27 +1,41 @@
 import React from 'react'
 import './searchResult.scss'
-import {Container, Row, Col} from 'react-bootstrap';
-
+import {Container, Card, Button} from 'react-bootstrap';
 
 const Property = ({property}) =>{
-    console.log(property)
     return (
-        <Row>
-            <Col>
-            <img src={property.coverPhoto.url} className='img-thumbnail' alt='house'/>
-            </Col>
-        </Row>
-    )
-}
+        <div>
+            <Card style={{display: 'flex'}}>
+                <Card.Img className='' variant="top" src={property.coverPhoto.url} 
+                style={{maxHeight:'200px'}}/>
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+        </div>
+        )
+    }
 
 const SearchResult = ({forRent, forSale}) => {
 
     return (
         <div className='searchResult'>
-            <Container>
-                {forRent.map((property) =>{
-                    return <Property property={property}/>
-                })}
+            <Container style={{display:'grid', gridTemplateColumns:'1fr 1fr 1fr', gap:'5px'}}>
+                {forRent.map((property, index) =>{
+                    return (
+                    
+                                <div >
+                                    <Property property={property}/>
+                                </div>
+    
+                        )
+                    })
+                }
             </Container>
         </div>
     )
